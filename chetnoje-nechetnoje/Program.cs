548b8a321557ce41238bba69508f827a550b8590
Martin <rauch83@yandex.ru>
2022-08-06 02:08:45 +0300

    zadanije 3

---
+++ chetnoje-nechetnoje/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.Write(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.Write(" нечетное");
+}
+
+
+
