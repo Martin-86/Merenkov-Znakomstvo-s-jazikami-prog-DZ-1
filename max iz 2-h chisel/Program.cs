bae3c644cda41d8469124a7cbd60a76e0c8e9000
Martin <rauch83@yandex.ru>
2022-08-06 00:15:31 +0300

    zadanije 1

--- max iz 2-h chisel/Program.cs
+++ max iz 2-h chisel/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Max:"); 
+
+if (number1>number2)
+    {
+        Console.WriteLine(number1);
+    }
+Console.WriteLine(number2);
