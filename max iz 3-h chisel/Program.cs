3a4f39b1c28876db8678ce3c5049a0d55fa9c029
Martin <rauch83@yandex.ru>
2022-08-06 00:30:16 +0300

    zadanije 2

---
+++ max iz 3-h chisel/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 3");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int a = number1;
+int b = number2;
+int c = number3;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+
+Console.Write("max = ");
+Console.Write(max);
